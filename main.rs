b987cf3dfe860643affe48fb7975858460056ef8
NetworkSofifi <122989903+NetworkSofifi@users.noreply.github.com>
2025-02-04 09:57:23 -0600

    rust01-rustc-review01

--- main.rs
+++ main.rs
@@ -1,10 +1,21 @@
 //
-// Hello World - main.rs
-/* Alternate comment style */
+// reviewreview01   main.rs
+//
 
 fn main() {
-    println!("\tHello, world!\n");
+    
+    let n : u32 = 5;   // Explicit type specification
 
-    print!("\tHello, ");                  // No newline added
-    println!("world!\n");
+    let m = 10_i32;    // Context for type inference
+
+    let x;             // Split declaration and initialization
+    x = 15;            // Type inferred by context
+    
+    let mut y : i32 = 20;             // Error1 - no type specification - add : i32
+                       // Error2 - uninitialized variable - add = 20 
+
+    println!("n = { } and m = { }", n, m);    // similar to C printf
+
+    print!("(x,y) = ");        // Does not add newline
+    println!("({x}, {y})");    // Adds newline
 }
