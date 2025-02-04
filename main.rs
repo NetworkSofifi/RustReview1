b18bd2edfc3dae7f3c1d50b35089f2b2408bef26
NetworkSofifi <122989903+NetworkSofifi@users.noreply.github.com>
2025-02-04 09:56:38 -0600

    rust01-rustc

---
+++ main.rs
@@ -0,0 +1,10 @@
+//
+// Hello World - main.rs
+/* Alternate comment style */
+
+fn main() {
+    println!("\tHello, world!\n");
+
+    print!("\tHello, ");                  // No newline added
+    println!("world!\n");
+}
